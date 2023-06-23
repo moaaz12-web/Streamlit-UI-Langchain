6349e853da2352d554c8e23a3a47024ca24b9fd3
moaaz12-web <moaaz7151@gmail.com>
2023-06-24 02:24:13 +0500

    Updated

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
+from langchain.chains.question_answering import load_qa_chain
+from langchain.document_loaders import DirectoryLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Pinecone
+import os
+from dotenv import load_dotenv
+import nltk
+import streamlit as st
+import pinecone
 from dotenv import load_dotenv
 import pickle
 from PyPDF2 import PdfReader
@@ -48,12 +58,8 @@ def main():
         for page in pdf_reader.pages:
             text += page.extract_text()
 
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-        )
-        chunks = text_splitter.split_text(text=text)
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.split_text(text)
 
         # Extract the store name from the PDF path
         store_name = os.path.splitext(os.path.basename(pdf_path))[0]
@@ -61,23 +67,39 @@ def main():
 
         if os.path.exists(f"{store_name}.pkl"):
             with open(f"{store_name}.pkl", "rb") as f:
-                VectorStore = pickle.load(f)
+                docsearch = pickle.load(f)
         else:
             embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
             VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+
+            pinecone.init(
+            api_key=os.environ.get("PINECONE_API_KEY"),  # Replace with your Pinecone API key
+            environment=os.environ.get("PINECONE_API_ENV")  # Replace with your Pinecone API environment
+            )
+            index_name = "moaaz"
+
+            # Create the vectorstore using Pinecone
+            docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name=index_name)
+
+
             with open(f"{store_name}.pkl", "wb") as f:
-                pickle.dump(VectorStore, f)
+                pickle.dump(docsearch, f)
 
         # Accept user questions/query
         query = st.text_input("Ask questions about your PDF file:")
 
         if query:
-            docs = VectorStore.similarity_search(query=query, k=3)
+            qa = load_qa_chain(llm=OpenAI(model_name='text-davinci-002'))
 
-            llm = OpenAI(openai_api_key=os.environ.get("OPENAI_API_KEY"))
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            docs = docsearch.similarity_search(query, include_metadata=True)
+
+            
             with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=query)
+                response = qa.run( input_documents=docs,
+                    question={
+                              "query": f"Answer the following question in a humanly, friendly tone. Moreover, answer only in the language the question is asked in. Do not answer like a robot. Thoroughly understand the question and answer only in the specified language:\n{query}"
+                    }
+    )
                 print(cb)
             st.write(response)
 
