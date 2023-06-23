b07bd3424228a3dc2797f6ce7b39291f227797e9
moaaz12-web <moaaz7151@gmail.com>
2023-06-23 23:41:28 +0500

    Initial commit

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
- 
 import streamlit as st
 from dotenv import load_dotenv
 import pickle
 from PyPDF2 import PdfReader
-# from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
@@ -12,82 +10,74 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
 import os
 import openai
+import requests
 
 load_dotenv()
 
-
 from langchain.chains import VectorDBQA
- 
-# Sidebar contents
-with st.sidebar:
-    st.title('🤗💬 LLM Chat App')
-    st.markdown('''
-    ## About
-    This app is an LLM-powered chatbot built using:
-    - [Streamlit](https://streamlit.io/)
-    - [LangChain](https://python.langchain.com/)
-    - [OpenAI](https://platform.openai.com/docs/models) LLM model
- 
-    ''')
-    # add_vertical_space(5)
-    st.write('Made with ❤️ by [Moaaz Siddiqui]')
- 
- 
+
+def download_pdf():
+    # Specify the URL of the PDF file to download
+    pdf_url = "https://public.railinc.com/sites/default/files/documents/ShipmentConditions.pdf"
+
+    # Send a GET request to download the PDF
+    response = requests.get(pdf_url)
+
+    # Save the PDF locally
+    with open("downloaded_pdf.pdf", "wb") as f:
+        f.write(response.content)
+
 def main():
     st.header("Chat with PDF 💬")
- 
- 
-    # upload a PDF file
-    pdf = st.file_uploader("Upload your PDF", type='pdf')
- 
-    # st.write(pdf)
-    if pdf is not None:
-        pdf_reader = PdfReader(pdf)
-        
+
+    # Add a button to trigger the PDF update
+    if st.button("Update PDF"):
+        st.write("Downloading the PDF...")
+        download_pdf()
+        st.write("PDF has been downloaded.")
+    # Load the downloaded PDF
+    pdf_path = "downloaded_pdf.pdf"
+    if not os.path.exists(pdf_path):
+        st.write("PDF not found. Click the 'Update PDF' button to download.")
+    else:
+        pdf_reader = PdfReader(pdf_path)
+       
         text = ""
         for page in pdf_reader.pages:
             text += page.extract_text()
- 
+
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=1000,
             chunk_overlap=200,
             length_function=len
-            )
+        )
         chunks = text_splitter.split_text(text=text)
- 
-        # # embeddings
-        store_name = pdf.name[:-4]
+
+        # Extract the store name from the PDF path
+        store_name = os.path.splitext(os.path.basename(pdf_path))[0]
         st.write(f'{store_name}')
-        # st.write(chunks)
- 
+
         if os.path.exists(f"{store_name}.pkl"):
             with open(f"{store_name}.pkl", "rb") as f:
                 VectorStore = pickle.load(f)
-            # st.write('Embeddings Loaded from the Disk')s
         else:
             embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
             VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
             with open(f"{store_name}.pkl", "wb") as f:
                 pickle.dump(VectorStore, f)
- 
-        # embeddings = OpenAIEmbeddings()
-        # VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
- 
+
         # Accept user questions/query
         query = st.text_input("Ask questions about your PDF file:")
-        # st.write(query)
- 
+
         if query:
             docs = VectorStore.similarity_search(query=query, k=3)
- 
+
             llm = OpenAI(openai_api_key=os.environ.get("OPENAI_API_KEY"))
             chain = load_qa_chain(llm=llm, chain_type="stuff")
-            # chain = VectorDBQA.from_chain_type(llm, chain_type="stuff", vectorstore=VectorStore)
             with get_openai_callback() as cb:
                 response = chain.run(input_documents=docs, question=query)
-                # response = chain.run(query)
                 print(cb)
             st.write(response)
- 
+
 if __name__ == '__main__':
     main()
--- apps.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-print(type(os.environ.get("OPENAI_API_KEY")))
